f14878556c226813bb8bec22cdfc86235df7efcc
FuadHr <foadh1988@gmail.com>
2021-12-11 00:34:48 +0330

    rest

--- src/main/java/com/sample/mysite/MysiteApplication.java
+++ src/main/java/com/sample/mysite/MysiteApplication.java
@@ -2,8 +2,10 @@ package com.sample.mysite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan("com.sample.mysite")
 public class MysiteApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/sample/mysite/login/controller/login.java
@@ -0,0 +1,18 @@
+package com.sample.mysite.login.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+
+@RestController()
+@RequestMapping("/login")
+public class login {
+
+    @PostMapping("/test/{id}")
+    public void login(@PathVariable int id){
+        ResponseEntity.ok(id);
+    }
+
+    //@PostMapping("/test2")
+}
